bd5b0008ca6b152117d1eb61e8ca4a57166e623f
Cipher Sage <ciphersagecode@gmail.com>
2025-07-23 20:58:30 +0300

    breaking: non descriptive error responses

--- userandorder/error/error_handler.py
+++ userandorder/error/error_handler.py
@@ -3,6 +3,8 @@ from userandorder.models.base_response_model import BaseAPIResponse
 from userandorder.utils.network_response import error_response
 from userandorder.utils.http_status_messages import HTTP_STATUS_MESSAGES
 
-def get_error_response(status_code: int = 500, _message: str = None) -> BaseAPIResponse:
+from typing import Optional
+
+def get_error_response(status_code: int = 500, _message: Optional[str] = None) -> BaseAPIResponse:
     message = HTTP_STATUS_MESSAGES.get(status_code, "An unknown error occurred.")
     return error_response(message=_message if _message else message, error=True, status_code=status_code)
